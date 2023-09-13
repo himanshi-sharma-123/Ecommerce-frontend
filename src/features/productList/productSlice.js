9761a72f9bb1f13d5e138c2c8d95bf89aa77a1a6
Himanshi Sharma <himanshish456@gmail.com>
2023-09-13 11:40:48 +0530

    product details page

--- src/features/productList/productSlice.js
+++ src/features/productList/productSlice.js
@@ -4,6 +4,7 @@ import {
   fetchProductsByFilters,
   fetchBrands,
   fetchCategories,
+  fetchProductById,
 } from "./productAPI";
 
 const initialState = {
@@ -12,12 +13,21 @@ const initialState = {
   categories: [],
   status: "idle",
   totalItems: 0,
+  selectedProduct: null,
 };
 
 export const fetchAllProductsAsync = createAsyncThunk(
   "product/fetchAllProducts",
   async () => {
     const response = await fetchAllProducts();
+    return response.data;
+  }
+);
+
+export const fetchAllProductsByIdAsync = createAsyncThunk(
+  "product/fetchProductById",
+  async (id) => {
+    const response = await fetchProductById(id);
     return response.data;
   }
 );
@@ -90,6 +100,14 @@ export const productSlice = createSlice({
         state.status = "idle";
         state.categories = action.payload;
         // state.products = [...state.products, ...action.payload];
+      })
+      .addCase(fetchAllProductsByIdAsync.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchAllProductsByIdAsync.fulfilled, (state, action) => {
+        state.status = "idle";
+        state.selectedProduct = action.payload;
+        // state.products = [...state.products, ...action.payload];
       });
   },
 });
@@ -98,6 +116,8 @@ export const productSlice = createSlice({
 export const selectAllProducts = (state) => state.product.products;
 export const selectBrands = (state) => state.product.brands;
 export const selectCategories = (state) => state.product.categories;
+export const selectProductById = (state) => state.product.selectedProduct;
+
 export const selectTotalItems = (state) => state.product.totalItems;
 
 export default productSlice.reducer;
