860ce9edab9afcba9e49b1118bf3e3ecb9e12634
Himanshi Sharma <himanshish456@gmail.com>
2023-09-18 13:28:55 +0530

    my orders added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SignupPage from "./pages/SignupPage";
 import CartPage from "./pages/CartPage";
 import Checkout from "./pages/Checkout";
 import ProductDetailPage from "./pages/ProductDetailPage";
+import UserOrders from "./features/user/components/UserOrders";
 
 import {
   createBrowserRouter,
@@ -20,6 +21,7 @@ import { fetchItemsByUserIdAsync } from "./features/cart/cartSlice";
 import { selectLoggedInUser } from "./features/auth/authSlice";
 import PageNotFound from "./pages/404";
 import OrderSuccessPage from "./pages/OrderSuccessPage";
+import UserOrdersPage from "./pages/UserOrdersPage";
 
 const router = createBrowserRouter([
   {
@@ -65,6 +67,11 @@ const router = createBrowserRouter([
   {
     path: "/order-success/:id",
     element: <OrderSuccessPage></OrderSuccessPage>,
+  },
+  {
+    path: "/orders",
+    element: <UserOrdersPage></UserOrdersPage>,
+    // we will add page later , right now using component directly
   },
 
   {
